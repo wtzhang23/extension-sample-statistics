f5901d7f1a802ebccd7c007bfe9e46ca801152b7
William Zhang <wtzhang2@cs.utexas.edu>
2020-04-21 23:10:32 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# extension-sample-statistics
+`ess.py` is a script to get statistics for numbers written in files with a certain extension. The script reads a lone floating point or integer value stored a file for every file and computes the mean, standard deviation, first quartile, second quartile and median values. It also supports graphing distributions for visual analysis.
+
+# Using the script.
+## Prerequisites 
+* Python 3
+* Matplotlib and Numpy
+
+## Running the script
+It may be helpful in Unix systems to make the script executable. Run `chmod +x ess.py` to do so.
+
+For all optional and positional arguments and their functions, run `ess --help`.
+  
+To view the sample statistics of files with a certain extension, run `ess.py E` where `E` is the file extension to analyze. All files with that extension are then used to determine the sample statistics.
+
+To plot a distribution graph, run `ess.py -p P E` where `P` is the type of graph you would like to plot and `E` is the file extension to analyze. This will write the plot to `x.png` by default. Check out the other flags for more customization.
+
+## Common Errors
+* **I'm getting a "no display name and no $DISPLAY environment variable" error:** Matplotlib relies on a backend to draw its graphs. Your backend relies on having a display. If you are using this command through `ssh` try X11 forwarding or using a backend that does not need a display such as Agg. You can set the backend used by matplotlib by passing its name into the `-b` argument.  
+* **I'm getting a "no such file or directory" error:** At this point, this script does not support creating directories that do not exist.
--- ess.py
+++ ess.py
@@ -14,41 +14,51 @@ def main():
     matplotlib = importlib.import_module('matplotlib')
     np = importlib.import_module('numpy')
 
+    graph_types = ('NONE', 'HISTOGRAM', 'BOXBLOT', 'N', 'H', 'B')
+
     parser = argparse.ArgumentParser(description='Get sample statistics for file reads with a certain extension. The files must only consist of one number.')
+    parser._actions[0].help = 'Show this message and exit.'
     parser.add_argument('extension', action='store', metavar='E', type=str, help='the file extension')
-    parser.add_argument('--force', '-f', action='store_true', help='Forces the program to continue executing instead of prompting.')
-    parser.add_argument('--verbose', '-v', action='store_true', help='Show all error messages.')
-    parser.add_argument('--plot-type', '-p',
+    parser.add_argument('-f', '--force', action='store_true', help='Forces the program to continue executing instead of prompting.')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Show all error messages.')
+    parser.add_argument('-p', '--plot-type',
             dest='type',
             action='store',
-            default='none', 
-            choices=('none', 'histogram', 'boxplot', 'n', 'h', 'b'),
-            help='The type of graph to plot.')
-    parser.add_argument('--output-path', '-o',
+            metavar='P',
+            type=str.upper,
+            default='NONE', 
+            choices=('NONE', 'HISTOGRAM', 'BOXBLOT', 'N', 'H', 'B'),
+            help=f'The type of graph to plot. The supported graphs are histograms and boxplots. Valid arguments: {graph_types}.')
+    parser.add_argument('-o', '--output-path',
             dest='filename',
             action='store',
+            metavar='F',
             default='x.png',
             help='The file name of the plot.')
-    parser.add_argument('--title', '-t',
+    parser.add_argument('-t', '--title',
             dest='title',
             action='store',
+            metavar='T',
             default='distribution',
             help='The title of the plot.')
-    parser.add_argument('--x-label', '-x',
+    parser.add_argument('-x', '--x-label',
             dest='xlabel',
             action='store',
+            metavar='X',
             default='values',
             help='The x-axis label.')
-    parser.add_argument('--y-label', '-y',
+    parser.add_argument('-y', '--y-label',
             dest='ylabel',
             action='store',
+            metavar='Y',
             default=None,
             help='The y-axis label.')
-    parser.add_argument('--backend', '-b',
+    parser.add_argument('-b', '--backend',
             dest='backend',
+            metavar='B',
             action='store',
             default=matplotlib.get_backend(),
-            help=f'The backend to be used by matplotlib. By default, {matplotlib.get_backend()}.')
+            help=f'The backend to be used by matplotlib for plot generation. By default, {matplotlib.get_backend()} is used.')
     args = parser.parse_args()
 
     matplotlib.use(args.backend)
@@ -59,7 +69,7 @@ def main():
         print(f'[status] sampling from files with the extension .{ext}.')
     def f(fp) :
         idx = fp.find(ext)
-        return idx == len(fp) - len(ext) and idx > 1 and fp[idx - 1] == '.' # idx > 1 to ensure file has a name and is not a hidden file
+        return idx == len(fp) - len(ext) and idx > 0 and fp[idx - 1] == '.'
 
     data = []
     for file_path in filter(f, os.listdir('.')):
@@ -83,15 +93,18 @@ def main():
     print(f'mean: {mean:.3f}', f'std: {std:.3f}')
     print(f'median: {med:.3f}', f'1st quartile: {fq:.3f}', f'2nd quartile: {sq:.3f}', f'iqr: {iqr:.3f}')
 
-    if args.type[0] != 'n':
-        if args.type[0] == 'h':
-            plt.hist(arr)
-        elif args.type[0] == 'b':
-            plt.boxplot(arr, vert = False)
-        plt.title(args.title)
-        plt.xlabel(args.xlabel)
-        if args.ylabel is not None:
-            plt.ylabel(args.ylabel)
+    if args.type[0] != 'N':
+        try:
+            if args.type[0] == 'H':
+                plt.hist(arr)
+            elif args.type[0] == 'B':
+                plt.boxplot(arr, vert = False)
+            plt.title(args.title)
+            plt.xlabel(args.xlabel)
+            if args.ylabel is not None:
+                plt.ylabel(args.ylabel)
+        except Exception as e:
+            print(f'[error] {e}')
         
         save_file = True
         if os.path.isfile(args.filename) and not args.force:
@@ -102,9 +115,12 @@ def main():
             if ipt == 'n' or ipt == 'N':
                 save_file = False
         if save_file:
-            plt.savefig(args.filename)
-            if args.verbose:
-                print(f'[status] saved plot at {os.path.abspath(args.filename)}.')
+            try:
+                plt.savefig(args.filename)
+                if args.verbose:
+                    print(f'[status] saved plot at {os.path.abspath(args.filename)}.')
+            except IOError as io:
+                print(f'[error] {io}.')
 
 if __name__ == "__main__":
     try:
@@ -117,6 +133,6 @@ if __name__ == "__main__":
             path = f'fp{idx}.log'
         print(f'[error] dumped unhandled error into {os.path.abspath(path)}.')
         with open(path, 'w') as f:
-            f.write(str(e))
+            f.write(e)
             f.write(traceback.format_exc())
         
